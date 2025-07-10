8d9d3b9166f53fb110db7a08b1cfa4ae49d587e8
Calin Iorga <caliniorga7@gmail.com>
2025-07-10 17:29:48 +0300

    started working on service

--- src/service/TaskService.java
+++ src/service/TaskService.java
@@ -1,4 +1,16 @@
 package service;
 
+import model.Priority;
+import model.Status;
+import model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskService {
+    private List<Task> tasks;
+    public TaskService(){
+        this.tasks = new ArrayList<>();
+    }
+
 }
